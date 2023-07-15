aaed3e158f3a11816dc18256c4abe836746f3d6f
ityuany <519495771@qq.com>
2023-07-16 02:39:54 +0800

    debug

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,32 +1,34 @@
-import { $ } from 'execa';
-import process from 'node:process';
-import readYamlFile from 'read-yaml-file';
-import path from 'node:path';
-import { findPackages } from 'find-packages';
-import { readPackage } from 'read-pkg';
-import { writePackage } from 'write-pkg';
+import { $ } from "execa";
+import process from "node:process";
+import readYamlFile from "read-yaml-file";
+import path from "node:path";
+import { findPackages } from "find-packages";
+import { readPackage } from "read-pkg";
+import { writePackage } from "write-pkg";
 
 const gitTag = process.env.CI_COMMIT_TAG!;
+
+console.log(process.env);
 
 if (gitTag) {
   function extractVersionTag(version: string): string {
     const match = /^v?\d+\.\d+\.\d+(-([\w]+))?(\d*\.*[\w]+)?$/i.exec(version);
-    return match && match[2] ? match[2] : 'latest';
+    return match && match[2] ? match[2] : "latest";
   }
 
   const npmTag = extractVersionTag(gitTag);
 
-  if (npmTag === 'snapshot') {
+  if (npmTag === "snapshot") {
     const yaml = await readYamlFile<{ packages: string[] }>(
-      path.join(process.cwd(), 'pnpm-workspace.yaml'),
+      path.join(process.cwd(), "pnpm-workspace.yaml")
     );
     const packagesMeta = await findPackages(process.cwd(), {
       patterns: yaml.packages,
     });
     for (const iterator of packagesMeta) {
       const json = await readPackage({ cwd: iterator.dir });
-      json.version = gitTag.replace(/^v/i, '');
-      await writePackage(path.join(iterator.dir, 'package.json'), json);
+      json.version = gitTag.replace(/^v/i, "");
+      await writePackage(path.join(iterator.dir, "package.json"), json);
     }
   }
   try {
