9afa96aef73ae236c4054c252c11030858bc22d0
Viki <hi@viki.moe>
2023-07-21 15:18:03 +0800

    test: refine tests in proxy & index

--- packages/reactive/src/index.spec.ts
+++ packages/reactive/src/index.spec.ts
@@ -1,16 +1,16 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, vitest } from "vitest";
 import { create, proxy, subscribe, useSnapshot } from "./index";
 
 describe("index", () => {
   it("create, proxy, useSnapshot and subscribe should be defined", () => {
     expect(create).toBeDefined();
-
+    
     expect(proxy).toBeDefined();
     expect(subscribe).toBeDefined();
     expect(useSnapshot).toBeDefined();
   });
 
-  it("should be return current, useSnapshot and subscribe by current state", () => {
+  it("should return current, useSnapshot and subscribe by current state", () => {
     const store = create({
       name: "Pikachu",
       address: {
@@ -25,7 +25,15 @@ describe("index", () => {
       },
     });
 
-    expect(store.useSnapshot).toBeDefined();
-    expect(store.subscribe).toBeDefined();
+    const snapshot = store.useSnapshot();
+    expect(snapshot).toEqual(store.current);
+
+    const callback = vitest.fn();
+    store.subscribe(callback);
+    expect(callback).toHaveBeenCalledTimes(0);
+
+    // 修改状态后，验证订阅回调被调用
+    store.current.name = "Charmander";
+    expect(callback).toHaveBeenCalledTimes(1);
   });
 });
