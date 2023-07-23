069b30aa6367626342b0ca2b4a54faaec54b6a5e
ityuany <519495771@qq.com>
2023-07-23 13:25:45 +0800

    create returnType to Readonly

--- packages/reactive/src/index.ts
+++ packages/reactive/src/index.ts
@@ -3,7 +3,13 @@ import { useSnapshot } from "./use-snapshot.js";
 import { subscribe } from "./subscribe.js";
 import { DeepReadonly } from "./utils.js";
 
-export function create<T extends object>(initState: T) {
+export function create<T extends object>(
+  initState: T
+): Readonly<{
+  mutate: T;
+  useSnapshot: () => DeepReadonly<T>;
+  subscribe: (callback: () => void) => () => void;
+}> {
   const state = proxy(initState);
   return {
     mutate: state,
