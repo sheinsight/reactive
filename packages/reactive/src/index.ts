15076cda95af91e8bb19425f5175185248f691fa
Viki <hi@viki.moe>
2023-07-25 10:43:28 +0800

    feat: add config for devtool

--- packages/reactive/src/index.ts
+++ packages/reactive/src/index.ts
@@ -10,7 +10,26 @@ export type CreateReturn<T extends object> = Readonly<{
   restore: () => void;
 }>;
 
-export function create<T extends object>(initState: T): CreateReturn<T> {
+/** initial options for creation */
+export interface CreateOptions {
+  /** devtool options, if set, will enable redux devtool */
+  devtool?: {
+    /**
+     * name of the store, will be displayed in devtool panel
+     */
+    name: string;
+    /**
+     * if set to true, will enable forever whenever production or not
+     * @default false
+     * */
+    forceEnable?: boolean;
+  };
+}
+
+export function create<T extends object>(
+  initState: T,
+  options?: CreateOptions
+): CreateReturn<T> {
   const state = proxy(initState);
   return {
     mutate: state,
