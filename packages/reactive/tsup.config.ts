24aa64b6296cba8f156e39b149d8bd2439c5dd43
ityuany <519495771@qq.com>
2023-08-25 01:55:00 +0800

    Solve compatibility issues with older versions of browsers.

--- packages/reactive/tsup.config.ts
+++ packages/reactive/tsup.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'tsup'
+import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  target: "es5",
+  entry: ["src/index.ts"],
   splitting: false,
   sourcemap: true,
   clean: true,
   dts: true,
-  format: 'esm'
-})
+  format: "esm",
+});
