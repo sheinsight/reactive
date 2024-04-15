b2704a3e6aeced3d21608020099064e87d0ea401
Viki <hi@viki.moe>
2024-04-15 15:11:59 +0800

    chore: config docs action & add peerDepOption

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/reactive',
   title: 'Reactive',
   description: 'Proxy-driven state for React & vanilla JS.',
   themeConfig: {
