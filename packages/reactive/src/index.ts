3005151225ee36cb3e94f03ef2d441cff8b850c3
Viki <hi@viki.moe>
2024-07-30 11:19:58 +0800

    feat: export `subscribe`, `getSnapshot`, `produce` from root

--- packages/reactive/src/index.ts
+++ packages/reactive/src/index.ts
@@ -1,4 +1,4 @@
-export { ref, devtools } from './vanilla/index.js'
+export { ref, devtools, subscribe, getSnapshot, produce } from './vanilla/index.js'
 export { createWithHooks as create } from './react/index.js'
 
 export type * from './vanilla/index.js'
