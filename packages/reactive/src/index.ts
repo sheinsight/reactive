5b7f7a464a61d8a8d283b3f2d6125bb0fb4304ad
Viki <hi@viki.moe>
2024-07-30 11:19:15 +0800

    feat: export all types in reactive

--- packages/reactive/src/index.ts
+++ packages/reactive/src/index.ts
@@ -1,4 +1,6 @@
 export { ref, devtools } from './vanilla/index.js'
 export { createWithHooks as create } from './react/index.js'
 
-export type { Store } from './react/index.js'
+export type * from './vanilla/index.js'
+export type * from './react/index.js'
+export type * from './utils/index.js'
