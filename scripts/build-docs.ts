0feee49ee80010bbb8c1d11e198b24b6a17a73a6
Viki <hi@viki.moe>
2024-08-23 17:02:08 +0800

    docs: fix docs build scripts

--- scripts/build-docs.ts
+++ scripts/build-docs.ts
@@ -5,7 +5,7 @@ const isSoDoc = process.env.PIPLINENAME === 'SODOC'
 if (isSoDoc) {
   process.env.IS_SODOC = 'true'
 
-  await $`pnpm add @alita/rspress-plugin @shein/rspress-plugin-sodoc`
+  await $`pnpm add @alita/rspress-plugin @shein/rspress-plugin-sodoc -w`
 }
 
 await $`rspress build docs`
