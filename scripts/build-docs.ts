c290ef4a756f648d8f28335690a167db116f4736
Viki <hi@viki.moe>
2025-07-02 14:31:21 +0800

    chore: 修复模块名称安装错误

--- scripts/build-docs.ts
+++ scripts/build-docs.ts
@@ -5,7 +5,7 @@ const isSoDoc = process.env.PIPLINENAME === 'SODOC'
 if (isSoDoc) {
   process.env.IS_SODOC = 'true'
 
-  await $`pnpm add -Dw @alita/rspress-plugin@^1 @@shein/rspress-plugin-sodoc@0.0.4`
+  await $`pnpm add -Dw @alita/rspress-plugin@^1 @shein/rspress-plugin-sodoc@0.0.4`
 }
 
 await $`rspress build docs`
