1040efb8bdcc2b5bad9f05f96e8900f787d345ee
Viki <hi@viki.moe>
2025-07-02 14:23:07 +0800

    chore: 优化构建脚本

--- scripts/build-docs.ts
+++ scripts/build-docs.ts
@@ -5,7 +5,7 @@ const isSoDoc = process.env.PIPLINENAME === 'SODOC'
 if (isSoDoc) {
   process.env.IS_SODOC = 'true'
 
-  await $`pnpm add @alita/rspress-plugin @shein/rspress-plugin-sodoc -w`
+  await $`pnpm add -Dw @alita/rspress-plugin @shein/rspress-plugin-sodoc`
 }
 
 await $`rspress build docs`
